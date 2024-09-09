8a832d0c9e16d4ae1470889e94498aae66be6523
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-09-08 18:32:50 -0700

    Fix mixin missing remap=false

--- src/main/java/dev/shadowsoffire/gateways/mixin/SpawnerBlockEntityMixin.java
+++ src/main/java/dev/shadowsoffire/gateways/mixin/SpawnerBlockEntityMixin.java
@@ -7,9 +7,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
 
-@Mixin(SpawnerBlockEntity.class)
+@Mixin(value = SpawnerBlockEntity.class, remap = false)
 public class SpawnerBlockEntityMixin {
 
+    /**
+     * Used to allow the dropped mob spawners from the Gateway of the Hellish Fortress to work properly.
+     * Without this, they will lose all NBT data on placement.
+     */
     @Inject(at = @At("HEAD"), method = "onlyOpCanSetNbt()Z", require = 1, cancellable = true)
     private void gateways_allowSetNbt(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(false);
